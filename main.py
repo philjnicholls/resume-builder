e0c9e52bef8f1f6822a31f7e97dfef3abf8ab747
Phil Nicholls <phil.j.nicholls@gmail.com>
2020-08-30 21:31:56 +0300

    Added HTML templating and exporting to PDF

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from resume import Resume
+from resume_builder import resume_builder as rb
 
-resume = Resume(first_name='Phil', last_name='Nicholls')
+resume = rb.Resume(first_name='Phil', last_name='Nicholls')
 
-print(resume)
+print(resume.html())
+resume.pdf('resume.pdf')
--- resume_builder/resume_builder.py
+++ resume_builder/resume_builder.py
@@ -1,4 +1,5 @@
 from weasyprint import HTML
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 class Experience():
     pass
@@ -22,6 +23,16 @@ class Resume():
             f'{self.first_name} {self.last_name}'
         )
 
-    def pdf(self):
-        pass
+    def html(self):
+        env = Environment(
+                loader=PackageLoader('resume_builder', 'templates'),
+                autoescape=select_autoescape(['html', 'xml'])
+        )
+        template = env.get_template(self.template)
+        return template.render(first_name=self.first_name,
+                               last_name=self.last_name)
+
+    def pdf(self, file_path='resume.pdf'):
+        html = HTML(string=self.html())
+        html.write_pdf(file_path)
 
